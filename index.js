4c2ccb59762155f3b4e94b9be89227673dd123c0
Robert Gittings <rjgitting@gmail.com>
2022-03-26 17:57:03 -0400

    commit tree 27fb8694d54aa47b63c35123bc444d89497f9ffa

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const testVar = "hi"
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Flatiron School is the champion!";
+newHeader.className = "victory"; 
+newHeader.className = "victory";
